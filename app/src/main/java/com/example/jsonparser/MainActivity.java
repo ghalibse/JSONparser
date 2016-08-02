4e2597ff2da690a55e67dff61eb423f507200494
ghalibse <ghalibse@gmail.com>
2016-08-02 16:05:49 -0400

    Second Commit

--- app/src/main/java/com/example/jsonparser/MainActivity.java
+++ app/src/main/java/com/example/jsonparser/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.jsonparser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
+
+    private String jsonString = "[{\"name\":\"Juan\",\"age\":20,\"grade\":8.1},{\"name\":\"Miguel\",\"age\":23,\"grade\":8.3},{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doMagic(View view) {
+
+
+        try {
+            JSONArray jsonArray = new JSONArray(jsonString);
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+//                Log.d(TAG, "doMagic: " + i + " " +jsonArray.get(i));
+
+                JSONObject jsonObject = new JSONObject(jsonArray.get(i).toString());
+
+                String name =  jsonObject.getString("name");
+
+                Log.d(TAG, "doMagic: " + i + " " + name);
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
